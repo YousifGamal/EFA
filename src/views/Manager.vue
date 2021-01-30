393fb36c9d9293a4cb6b71760e1d4d359220df95
YousifGamal <yousifgahmed@gmail.com>
2021-01-31 00:25:12 +0200

    vue 2

---
+++ src/views/Manager.vue
@@ -0,0 +1,53 @@
+<template>
+    <b-container fluid class="bv-example-row">
+        <b-row>
+            <b-col cols="2">col-2</b-col>
+            <b-col cols="8">
+                  <b-card
+                    title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                  >
+                    <b-card-text>
+                      Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                  </b-card>
+
+            </b-col>
+            <b-col cols="2">col-2</b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Manager',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
