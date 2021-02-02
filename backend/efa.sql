db783a834e4f99f88c8bc1aa33ac35c92f72d906
yousefdawood <yocomohamed@yahoo.com>
2021-02-02 21:34:38 +0200

    solving query

--- backend/efa.sql
+++ backend/efa.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 13.1 (Ubuntu 13.1-1.pgdg18.04+1)
 -- Dumped by pg_dump version 13.1 (Ubuntu 13.1-1.pgdg18.04+1)
 
--- Started on 2021-02-02 20:23:02 EET
+-- Started on 2021-01-30 19:58:47 EET
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,7 +19,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 5 (class 2615 OID 24707)
+-- TOC entry 6 (class 2615 OID 16385)
 -- Name: efa; Type: SCHEMA; Schema: -; Owner: postgres
 --
 
@@ -33,50 +33,26 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 202 (class 1259 OID 24710)
+-- TOC entry 207 (class 1259 OID 16448)
 -- Name: linesman; Type: TABLE; Schema: efa; Owner: postgres
 --
 
+
 CREATE TABLE efa.linesman (
-    linesman_id integer NOT NULL,
-    linesman_name character varying(50) NOT NULL
+    linesman_id  SERIAL,
+    linesman_name varchar(50) NOT NULL
 );
 
 
 ALTER TABLE efa.linesman OWNER TO postgres;
 
 --
--- TOC entry 201 (class 1259 OID 24708)
--- Name: linesman_linesman_id_seq; Type: SEQUENCE; Schema: efa; Owner: postgres
---
-
-CREATE SEQUENCE efa.linesman_linesman_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE efa.linesman_linesman_id_seq OWNER TO postgres;
-
---
--- TOC entry 3044 (class 0 OID 0)
--- Dependencies: 201
--- Name: linesman_linesman_id_seq; Type: SEQUENCE OWNED BY; Schema: efa; Owner: postgres
---
-
-ALTER SEQUENCE efa.linesman_linesman_id_seq OWNED BY efa.linesman.linesman_id;
-
-
---
--- TOC entry 204 (class 1259 OID 24716)
+-- TOC entry 202 (class 1259 OID 16394)
 -- Name: match; Type: TABLE; Schema: efa; Owner: postgres
 --
 
 CREATE TABLE efa.match (
-    match_id integer NOT NULL,
+    match_id  SERIAL,
     stadium_id integer NOT NULL,
     home_team integer NOT NULL,
     away_team integer NOT NULL,
@@ -92,75 +68,25 @@ CREATE TABLE efa.match (
 ALTER TABLE efa.match OWNER TO postgres;
 
 --
--- TOC entry 203 (class 1259 OID 24714)
--- Name: match_match_id_seq; Type: SEQUENCE; Schema: efa; Owner: postgres
---
-
-CREATE SEQUENCE efa.match_match_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE efa.match_match_id_seq OWNER TO postgres;
-
---
--- TOC entry 3045 (class 0 OID 0)
--- Dependencies: 203
--- Name: match_match_id_seq; Type: SEQUENCE OWNED BY; Schema: efa; Owner: postgres
---
-
-ALTER SEQUENCE efa.match_match_id_seq OWNED BY efa.match.match_id;
-
-
---
--- TOC entry 206 (class 1259 OID 24723)
+-- TOC entry 206 (class 1259 OID 16440)
 -- Name: referee; Type: TABLE; Schema: efa; Owner: postgres
 --
 
 CREATE TABLE efa.referee (
-    referee_id integer NOT NULL,
-    referee_name character varying(50) NOT NULL
+    referee_id  SERIAL,
+    referee_name varchar(50) NOT NULL
 );
 
 
 ALTER TABLE efa.referee OWNER TO postgres;
 
 --
--- TOC entry 205 (class 1259 OID 24721)
--- Name: referee_referee_id_seq; Type: SEQUENCE; Schema: efa; Owner: postgres
---
-
-CREATE SEQUENCE efa.referee_referee_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE efa.referee_referee_id_seq OWNER TO postgres;
-
---
--- TOC entry 3046 (class 0 OID 0)
--- Dependencies: 205
--- Name: referee_referee_id_seq; Type: SEQUENCE OWNED BY; Schema: efa; Owner: postgres
---
-
-ALTER SEQUENCE efa.referee_referee_id_seq OWNED BY efa.referee.referee_id;
-
-
---
--- TOC entry 208 (class 1259 OID 24729)
+-- TOC entry 203 (class 1259 OID 16402)
 -- Name: reservation; Type: TABLE; Schema: efa; Owner: postgres
 --
 
 CREATE TABLE efa.reservation (
-    ticket_number integer NOT NULL,
+    ticket_number serial NOT NULL,
     user_id integer NOT NULL,
     match_id integer NOT NULL,
     seat_number integer NOT NULL
@@ -170,38 +96,13 @@ CREATE TABLE efa.reservation (
 ALTER TABLE efa.reservation OWNER TO postgres;
 
 --
--- TOC entry 207 (class 1259 OID 24727)
--- Name: reservation_ticket_number_seq; Type: SEQUENCE; Schema: efa; Owner: postgres
---
-
-CREATE SEQUENCE efa.reservation_ticket_number_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE efa.reservation_ticket_number_seq OWNER TO postgres;
-
---
--- TOC entry 3047 (class 0 OID 0)
--- Dependencies: 207
--- Name: reservation_ticket_number_seq; Type: SEQUENCE OWNED BY; Schema: efa; Owner: postgres
---
-
-ALTER SEQUENCE efa.reservation_ticket_number_seq OWNED BY efa.reservation.ticket_number;
-
-
---
--- TOC entry 210 (class 1259 OID 24735)
+-- TOC entry 204 (class 1259 OID 16422)
 -- Name: stadium; Type: TABLE; Schema: efa; Owner: postgres
 --
 
 CREATE TABLE efa.stadium (
-    stadium_id integer NOT NULL,
-    stadium_name character varying(50) NOT NULL,
+    stadium_id SERIAL,
+    stadium_name varchar(50) NOT NULL,
     rows integer NOT NULL,
     columns integer NOT NULL
 );
@@ -210,84 +111,34 @@ CREATE TABLE efa.stadium (
 ALTER TABLE efa.stadium OWNER TO postgres;
 
 --
--- TOC entry 209 (class 1259 OID 24733)
--- Name: stadium_stadium_id_seq; Type: SEQUENCE; Schema: efa; Owner: postgres
---
-
-CREATE SEQUENCE efa.stadium_stadium_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE efa.stadium_stadium_id_seq OWNER TO postgres;
-
---
--- TOC entry 3048 (class 0 OID 0)
--- Dependencies: 209
--- Name: stadium_stadium_id_seq; Type: SEQUENCE OWNED BY; Schema: efa; Owner: postgres
---
-
-ALTER SEQUENCE efa.stadium_stadium_id_seq OWNED BY efa.stadium.stadium_id;
-
-
---
--- TOC entry 212 (class 1259 OID 24741)
+-- TOC entry 205 (class 1259 OID 16432)
 -- Name: teams; Type: TABLE; Schema: efa; Owner: postgres
 --
 
 CREATE TABLE efa.teams (
-    team_id integer NOT NULL,
-    team_name character varying(50) NOT NULL
+    team_id  SERIAL,
+    team_name varchar(50) NOT NULL
 );
 
 
 ALTER TABLE efa.teams OWNER TO postgres;
 
 --
--- TOC entry 211 (class 1259 OID 24739)
--- Name: teams_team_id_seq; Type: SEQUENCE; Schema: efa; Owner: postgres
---
-
-CREATE SEQUENCE efa.teams_team_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE efa.teams_team_id_seq OWNER TO postgres;
-
---
--- TOC entry 3049 (class 0 OID 0)
--- Dependencies: 211
--- Name: teams_team_id_seq; Type: SEQUENCE OWNED BY; Schema: efa; Owner: postgres
---
-
-ALTER SEQUENCE efa.teams_team_id_seq OWNED BY efa.teams.team_id;
-
-
---
--- TOC entry 214 (class 1259 OID 24747)
+-- TOC entry 201 (class 1259 OID 16386)
 -- Name: user; Type: TABLE; Schema: efa; Owner: postgres
 --
 
 CREATE TABLE efa."user" (
-    user_id integer NOT NULL,
-    user_name character varying(50) NOT NULL,
-    password character varying(50) NOT NULL,
-    first_name character varying(50) NOT NULL,
-    last_name character varying(50) NOT NULL,
+    user_id  SERIAL,
+    user_name varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
     birth_date date NOT NULL,
     gender "char" NOT NULL,
-    city character varying(50) NOT NULL,
+    city varchar(50) NOT NULL,
     address text,
-    email character varying(50) NOT NULL,
+    email varchar(50) NOT NULL,
     role "char" NOT NULL,
     status integer NOT NULL
 );
@@ -296,88 +147,7 @@ CREATE TABLE efa."user" (
 ALTER TABLE efa."user" OWNER TO postgres;
 
 --
--- TOC entry 213 (class 1259 OID 24745)
--- Name: user_user_id_seq; Type: SEQUENCE; Schema: efa; Owner: postgres
---
-
-CREATE SEQUENCE efa.user_user_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE efa.user_user_id_seq OWNER TO postgres;
-
---
--- TOC entry 3050 (class 0 OID 0)
--- Dependencies: 213
--- Name: user_user_id_seq; Type: SEQUENCE OWNED BY; Schema: efa; Owner: postgres
---
-
-ALTER SEQUENCE efa.user_user_id_seq OWNED BY efa."user".user_id;
-
-
---
--- TOC entry 2867 (class 2604 OID 24713)
--- Name: linesman linesman_id; Type: DEFAULT; Schema: efa; Owner: postgres
---
-
-ALTER TABLE ONLY efa.linesman ALTER COLUMN linesman_id SET DEFAULT nextval('efa.linesman_linesman_id_seq'::regclass);
-
-
---
--- TOC entry 2868 (class 2604 OID 24719)
--- Name: match match_id; Type: DEFAULT; Schema: efa; Owner: postgres
---
-
-ALTER TABLE ONLY efa.match ALTER COLUMN match_id SET DEFAULT nextval('efa.match_match_id_seq'::regclass);
-
-
---
--- TOC entry 2870 (class 2604 OID 24726)
--- Name: referee referee_id; Type: DEFAULT; Schema: efa; Owner: postgres
---
-
-ALTER TABLE ONLY efa.referee ALTER COLUMN referee_id SET DEFAULT nextval('efa.referee_referee_id_seq'::regclass);
-
-
---
--- TOC entry 2871 (class 2604 OID 24732)
--- Name: reservation ticket_number; Type: DEFAULT; Schema: efa; Owner: postgres
---
-
-ALTER TABLE ONLY efa.reservation ALTER COLUMN ticket_number SET DEFAULT nextval('efa.reservation_ticket_number_seq'::regclass);
-
-
---
--- TOC entry 2872 (class 2604 OID 24738)
--- Name: stadium stadium_id; Type: DEFAULT; Schema: efa; Owner: postgres
---
-
-ALTER TABLE ONLY efa.stadium ALTER COLUMN stadium_id SET DEFAULT nextval('efa.stadium_stadium_id_seq'::regclass);
-
-
---
--- TOC entry 2873 (class 2604 OID 24744)
--- Name: teams team_id; Type: DEFAULT; Schema: efa; Owner: postgres
---
-
-ALTER TABLE ONLY efa.teams ALTER COLUMN team_id SET DEFAULT nextval('efa.teams_team_id_seq'::regclass);
-
-
---
--- TOC entry 2874 (class 2604 OID 24750)
--- Name: user user_id; Type: DEFAULT; Schema: efa; Owner: postgres
---
-
-ALTER TABLE ONLY efa."user" ALTER COLUMN user_id SET DEFAULT nextval('efa.user_user_id_seq'::regclass);
-
-
---
--- TOC entry 2876 (class 2606 OID 24755)
+-- TOC entry 2884 (class 2606 OID 16455)
 -- Name: linesman linesman_pkey; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -386,7 +156,7 @@ ALTER TABLE ONLY efa.linesman
 
 
 --
--- TOC entry 2884 (class 2606 OID 24757)
+-- TOC entry 2870 (class 2606 OID 16399)
 -- Name: match match_pkey; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -395,7 +165,7 @@ ALTER TABLE ONLY efa.match
 
 
 --
--- TOC entry 2886 (class 2606 OID 24759)
+-- TOC entry 2882 (class 2606 OID 16447)
 -- Name: referee referee_pkey; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -404,16 +174,7 @@ ALTER TABLE ONLY efa.referee
 
 
 --
--- TOC entry 2888 (class 2606 OID 24820)
--- Name: reservation reservation_match_id_seat_number_key; Type: CONSTRAINT; Schema: efa; Owner: postgres
---
-
-ALTER TABLE ONLY efa.reservation
-    ADD CONSTRAINT reservation_match_id_seat_number_key UNIQUE (match_id, seat_number);
-
-
---
--- TOC entry 2890 (class 2606 OID 24761)
+-- TOC entry 2872 (class 2606 OID 16409)
 -- Name: reservation reservation_pkey; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -422,7 +183,7 @@ ALTER TABLE ONLY efa.reservation
 
 
 --
--- TOC entry 2892 (class 2606 OID 24763)
+-- TOC entry 2874 (class 2606 OID 16421)
 -- Name: reservation reservation_user_id_match_id_seat_number_key; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -431,7 +192,7 @@ ALTER TABLE ONLY efa.reservation
 
 
 --
--- TOC entry 2894 (class 2606 OID 24765)
+-- TOC entry 2876 (class 2606 OID 16429)
 -- Name: stadium stadium_pkey; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -440,7 +201,7 @@ ALTER TABLE ONLY efa.stadium
 
 
 --
--- TOC entry 2896 (class 2606 OID 24767)
+-- TOC entry 2878 (class 2606 OID 16431)
 -- Name: stadium stadium_stadium_name_key; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -449,7 +210,7 @@ ALTER TABLE ONLY efa.stadium
 
 
 --
--- TOC entry 2898 (class 2606 OID 24769)
+-- TOC entry 2880 (class 2606 OID 16439)
 -- Name: teams teams_pkey; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -458,7 +219,7 @@ ALTER TABLE ONLY efa.teams
 
 
 --
--- TOC entry 2900 (class 2606 OID 24771)
+-- TOC entry 2862 (class 2606 OID 16393)
 -- Name: user user_pkey; Type: CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -467,7 +228,7 @@ ALTER TABLE ONLY efa."user"
 
 
 --
--- TOC entry 2877 (class 1259 OID 24772)
+-- TOC entry 2863 (class 1259 OID 16473)
 -- Name: fk_away_team; Type: INDEX; Schema: efa; Owner: postgres
 --
 
@@ -475,7 +236,7 @@ CREATE INDEX fk_away_team ON efa.match USING btree (away_team);
 
 
 --
--- TOC entry 2878 (class 1259 OID 24773)
+-- TOC entry 2864 (class 1259 OID 16461)
 -- Name: fk_home_team; Type: INDEX; Schema: efa; Owner: postgres
 --
 
@@ -483,7 +244,7 @@ CREATE INDEX fk_home_team ON efa.match USING btree (home_team);
 
 
 --
--- TOC entry 2879 (class 1259 OID 24774)
+-- TOC entry 2865 (class 1259 OID 16485)
 -- Name: fk_linesman_1; Type: INDEX; Schema: efa; Owner: postgres
 --
 
@@ -491,7 +252,7 @@ CREATE INDEX fk_linesman_1 ON efa.match USING btree (linesman_1);
 
 
 --
--- TOC entry 2880 (class 1259 OID 24775)
+-- TOC entry 2866 (class 1259 OID 16491)
 -- Name: fk_linesman_2; Type: INDEX; Schema: efa; Owner: postgres
 --
 
@@ -499,7 +260,7 @@ CREATE INDEX fk_linesman_2 ON efa.match USING btree (linesman_2);
 
 
 --
--- TOC entry 2881 (class 1259 OID 24776)
+-- TOC entry 2867 (class 1259 OID 16479)
 -- Name: fk_referee; Type: INDEX; Schema: efa; Owner: postgres
 --
 
@@ -507,7 +268,7 @@ CREATE INDEX fk_referee ON efa.match USING btree (main_referee);
 
 
 --
--- TOC entry 2882 (class 1259 OID 24777)
+-- TOC entry 2868 (class 1259 OID 16467)
 -- Name: fk_stadium; Type: INDEX; Schema: efa; Owner: postgres
 --
 
@@ -515,7 +276,7 @@ CREATE INDEX fk_stadium ON efa.match USING btree (stadium_id);
 
 
 --
--- TOC entry 2901 (class 2606 OID 24778)
+-- TOC entry 2887 (class 2606 OID 16468)
 -- Name: match match_away_team_fkey; Type: FK CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -524,7 +285,7 @@ ALTER TABLE ONLY efa.match
 
 
 --
--- TOC entry 2902 (class 2606 OID 24783)
+-- TOC entry 2885 (class 2606 OID 16456)
 -- Name: match match_home_team_fkey; Type: FK CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -533,7 +294,7 @@ ALTER TABLE ONLY efa.match
 
 
 --
--- TOC entry 2903 (class 2606 OID 24788)
+-- TOC entry 2888 (class 2606 OID 16480)
 -- Name: match match_linesman_1_fkey; Type: FK CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -542,7 +303,7 @@ ALTER TABLE ONLY efa.match
 
 
 --
--- TOC entry 2904 (class 2606 OID 24793)
+-- TOC entry 2889 (class 2606 OID 16486)
 -- Name: match match_linesman_2_fkey; Type: FK CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -551,7 +312,7 @@ ALTER TABLE ONLY efa.match
 
 
 --
--- TOC entry 2905 (class 2606 OID 24798)
+-- TOC entry 2890 (class 2606 OID 16474)
 -- Name: match match_main_referee_fkey; Type: FK CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -560,7 +321,7 @@ ALTER TABLE ONLY efa.match
 
 
 --
--- TOC entry 2906 (class 2606 OID 24803)
+-- TOC entry 2886 (class 2606 OID 16462)
 -- Name: match match_stadium_id_fkey; Type: FK CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -569,7 +330,7 @@ ALTER TABLE ONLY efa.match
 
 
 --
--- TOC entry 2907 (class 2606 OID 24808)
+-- TOC entry 2892 (class 2606 OID 16415)
 -- Name: reservation reservation_match_id_fkey; Type: FK CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -578,7 +339,7 @@ ALTER TABLE ONLY efa.reservation
 
 
 --
--- TOC entry 2908 (class 2606 OID 24813)
+-- TOC entry 2891 (class 2606 OID 16410)
 -- Name: reservation reservation_user_id_fkey; Type: FK CONSTRAINT; Schema: efa; Owner: postgres
 --
 
@@ -586,7 +347,7 @@ ALTER TABLE ONLY efa.reservation
     ADD CONSTRAINT reservation_user_id_fkey FOREIGN KEY (user_id) REFERENCES efa."user"(user_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
--- Completed on 2021-02-02 20:23:02 EET
+-- Completed on 2021-01-30 19:58:47 EET
 
 --
 -- PostgreSQL database dump complete
