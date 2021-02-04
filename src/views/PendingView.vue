be583f10f47b438d419e1917c061861d16297ef3
therubick <mohamed_ayman_07@hotmail.com>
2021-02-04 22:05:14 +0200

    fixing style issue

--- src/views/PendingView.vue
+++ src/views/PendingView.vue
@@ -55,12 +55,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #344d66;
-  margin-top: 60px;
-}
 </style>
